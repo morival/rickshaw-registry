f20fcd64a7198f694748d79ef15e40e080ff1cf5
morival <64006835+morival@users.noreply.github.com>
2021-09-21 18:19:08 +0100

    created icon, Sign in email and password, remember me checkbox

--- src/components/login.js
+++ src/components/login.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Grid, Paper } from '@mui/material';
+import { Avatar, Button, Grid, Paper, TextField } from '@mui/material';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 function Login() {
     
-    const paperStyle={padding:20, height:'70vh', width:280, margin:"20px auto"};
+    const paperStyle={padding:20, height:"70vh", width:280, margin:"20px auto"};
+    const avatarStyle={backgroundColor:"#41a9e1"};
     
     return(
         <Grid>
             <Paper elevation={10} style={paperStyle}>
-                Sign In
+                <Grid align="center">
+                    <Avatar style={avatarStyle}><LockOpenOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label="Email" variant="filled" fullWidth required />
+                <TextField label="Password" type="password" autoComplete="current-password" variant="filled" fullWidth required />
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
+                <Button type="submit" color="primary" variant="contained" fullWidth>Sign in</Button>
             </Paper>
         </Grid>
     )
 }
-
 
 export default Login;
