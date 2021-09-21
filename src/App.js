f5817736209f7702ef99a1dc1f4120699f515fdf
morival <64006835+morival@users.noreply.github.com>
2021-09-21 16:04:27 +0100

    login-form branch, created Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Rickshaw Registry</h1>
       </header>
+      
+      <Login/>
+
     </div>
   );
 }
