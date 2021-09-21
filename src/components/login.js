dc18b92719856dbbc3f070db9b34467453706b2e
morival <64006835+morival@users.noreply.github.com>
2021-09-21 23:52:44 +0100

    added log in button, link to forgoten password and sign up link

--- src/components/login.js
+++ src/components/login.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import { Avatar, Button, Grid, Paper, TextField } from '@mui/material';
+import { Avatar, Button, Grid, Link, Paper, TextField, Typography } from '@mui/material';
 import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 function Login() {
     
-    const paperStyle={padding:20, height:"70vh", width:280, margin:"20px auto"};
+    const paperStyle={padding:20, width:280, margin:"20px auto"};
     const avatarStyle={backgroundColor:"#41a9e1"};
+    const marginStyle={margin:"8px 0"};
     
     return(
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align="center">
-                    <Avatar style={avatarStyle}><LockOpenOutlinedIcon/></Avatar>
-                    <h2>Sign In</h2>
+                    <Avatar style={avatarStyle}>
+                        <LockOpenOutlinedIcon/>
+                    </Avatar>
+                    <h2>Rickshaw Registry</h2>
+                    {/* <h4>Log In</h4> */}
                 </Grid>
-                <TextField label="Email" variant="filled" fullWidth required />
-                <TextField label="Password" type="password" autoComplete="current-password" variant="filled" fullWidth required />
+                <TextField label="Email" variant="filled" size="small" style={marginStyle} fullWidth required />
+                <TextField label="Password" type="password" autoComplete="current-password" variant="filled" size="small" fullWidth required />
                 <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
-                <Button type="submit" color="primary" variant="contained" fullWidth>Sign in</Button>
+                <Button type="submit" color="primary" variant="contained" style={marginStyle} fullWidth>
+                    Log In
+                </Button>
+                <Typography align="center">
+                    <Link href="#" underline="none">Forgotten your password?</Link>
+                </Typography>
+            </Paper>
+            <Paper elevation={10} style={paperStyle}>
+                <Typography align="center">
+                    Don't have an account?
+                    <Link href="#" underline="none"> Sign up </Link>
+                </Typography>
             </Paper>
         </Grid>
     )
