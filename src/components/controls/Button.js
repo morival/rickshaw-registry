c16cbebd86effe8da652d1da8dceb2631355d859
morival <64006835+morival@users.noreply.github.com>
2021-10-28 14:38:15 +0100

    refactor the styles for buttons

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { Button as MuiButton } from '@mui/material'
-import { makeStyles } from '@mui/styles';
-
-
-const useStyles = makeStyles({
-    root: {
-        "&.css-1fu7jd5-MuiButtonBase-root-MuiButton-root" :{ 
-            margin: "8px 0"
-        }
-    }
-})
 
 
 export default function Button(props) {
-
-    const classes = useStyles();
     
     const {text, size, color, variant, onClick, ...other} = props
 
     return(
         <MuiButton
-        className={classes.root}
+        sx={{ my: 1 }}
         size={size || "normal"}
         color={color || "primary"}
         variant={variant || "contained"}
