5b514cefd5c7efe7c6d655121f80001dc09109a3
morival <64006835+morival@users.noreply.github.com>
2022-03-03 23:20:58 +0000

    current user's records shown in records container are showing the date and time they were created correctly

--- src/containers/RecordsContainer.js
+++ src/containers/RecordsContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import RecordsServices from '../services/RecordsServices';
 import { useAuth } from '../components/context/AuthContext';
 import Controls from '../components/controls/Controls';
@@ -10,20 +10,26 @@ import { Link } from 'react-router-dom';
 export default function RecordsContainer(params) {
     
 
+    // Formating Time & Date
+    function formatTD(date) {
+        const newDate = new Date(date);
+        return `recorded on ${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()} 
+        at ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
+    } 
+
 
     // Auth
     const { currentUser, records, setRecords } = useAuth()
 
-    // const recordsComponent = (user) => ()
 
     useEffect(() => {
         async function findRecords(user) {
             const allRecords = await RecordsServices.getAllRecords()
             const filteredRecords = allRecords.data.filter(element => element.user_id === user._id)
             setRecords(filteredRecords)
-            console.log(records)
         }
         findRecords(currentUser)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[currentUser])
 
     return (
@@ -46,18 +52,22 @@ export default function RecordsContainer(params) {
             color="success"
             component={Link} to={"/dashboard"}
             />
-            <Box>
-                <Paper align="center">
+            <Box sx={{ justifyContent: 'center', display: 'flex' }}>
+                <Paper 
+                sx={{ p: 1, maxWidth: '800px', width: '100%' }}
+                align="center"
+                >
                     <Typography variant='h6'>Your previous records</Typography>
                     <List>
                         {records.map((element, i) => 
-                        <ListItem>
-                            <Controls.Button
-                            text={element.record_date}
-                            fullWidth
-                            key={i}
-                            />
-                        </ListItem>
+                            <ListItem key={i}>
+                                <Controls.Button
+                                // text={element.record_date}
+                                text={formatTD(element.record_date)}
+                                fullWidth
+                                
+                                />
+                            </ListItem>
                         )}
                     </List>
                 </Paper>
